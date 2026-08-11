f9e87a69008320bc7b115ae0ee12493a4e8fa0bb
rajj884078-del <rajj884078@gmail.com>
2026-08-11 14:24:22 +0530

    updated interface

--- pdfchat.py
+++ pdfchat.py
@@ -4,7 +4,8 @@ from google import genai
 
 client = genai.Client(api_key=st.secrets["GEMINI_KEY"])
 
-st.title("Chat with your PDF")
+st.title("📄 Chat with your PDF")
+st.write("Apni koi bhi PDF upload karo aur uske baare mein sawaal poocho. AI aapki file padhke jawab dega.")
 
 uploaded_file = st.file_uploader("Apni PDF upload karo", type="pdf")
 
@@ -13,15 +14,17 @@ if uploaded_file:
 	text = ""
 	for page in reader.pages:
 		text = text + page.extract_text()
+	st.success("✅ PDF load ho gayi! Now ask question.")
 
 	que = st.text_input("ASK ABOUT PDF:")
 	
 	if que:
-		full_prompt = "Ye document ka content hai:\n" + text + "\n\nIske base par answer do: " + que
-		ans = client.models.generate_content(
-		model="gemini-3.6-flash",
-		contents=full_prompt
+		with st.spinner("thinking..."):		
+			full_prompt = "Ye document ka content hai:\n" + text + "\n\nIske base par answer do: " + que
+			ans = client.models.generate_content(
+				model = "gemini-3.6-flash",
+				contents = full_prompt
 		)
-		st.write(ans.text)
+			st.write(ans.text)
 	
 
