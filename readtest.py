8283d4717525de0ac6df7d6851fb354861754422
rajj884078-del <rajj884078@gmail.com>
2026-08-17 12:45:41 +0530

    all codes under if uploaded pdf

---
+++ readtest.py
@@ -0,0 +1,6 @@
+f = open("mychat.txt")
+old_chat = f.read()
+f.close
+
+print("old chat:")
+print(old_chat)
---
+++ writetest.py
@@ -0,0 +1,5 @@
+f = open("mychat.txt", "w")
+f.write("User: hello\nBot: hi kaise ho\n")
+f.close()
+
+print("File mein likh diya!")
